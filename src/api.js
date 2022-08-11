7433da42f5b69dc7ba3ec5d081daaf642511473f
gmarzo <garrett.marzo@gmail.com>
2022-08-10 17:01:57 -0700

    Added point of interest functionality

--- src/api.js
+++ src/api.js
@@ -8,9 +8,19 @@ const getMapByAddress = async address => {
   const response = await fetch(
     `https://api.mapbox.com/geocoding/v5/mapbox.places/${
       address ? formattedAddress : defaultAddress
-    }.json?&access_token=${ACCESS_TOKEN}`
+    }.json?&access_token=${ACCESS_TOKEN}&limit=1`
   )
   return response.json()
 }
 
-export { getMapByAddress, ACCESS_TOKEN }
+const getMapByPOI = async (category, address) => {
+  const locations = await getMapByAddress(address)
+  const location = locations.features[0].geometry.coordinates
+
+  const response = await fetch(
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${category}.json?type=poi&proximity=${location}&access_token=${ACCESS_TOKEN}&limit=10`
+  )
+  return response.json()
+}
+
+export { getMapByAddress, getMapByPOI, ACCESS_TOKEN }
--- src/pages/ByPOI.jsx
+++ src/pages/ByPOI.jsx
@@ -16,7 +16,7 @@ import mapboxgl from 'mapbox-gl'
 
 import CATEGORIES from '../common/categories'
 
-import { getMapByAddress, ACCESS_TOKEN } from '../api'
+import { getMapByAddress, getMapByPOI, ACCESS_TOKEN } from '../api'
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
 mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default
@@ -35,12 +35,29 @@ const useStyles = makeStyles(
         'linear-gradient(32deg, rgba(78,67,255,1) 0%, rgba(128,128,255,1) 24%, rgba(0,212,255,1) 100%)',
     },
 
+    searchContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      width: '80vw',
+      height: '80vh',
+    },
+
     categorySelect: {
       backgroundColor: '#ffffff',
       border: '2px solid #000000',
       justifyContent: 'center',
       alignItems: 'center',
-      minWidth: '40vw',
+      minWidth: '70vw',
+    },
+
+    addressInput: {
+      backgroundColor: '#ffffff',
+      border: '2px solid #000000',
+      justifyContent: 'flex-start',
+      alignItems: 'flex-start',
+      width: '70vw',
     },
   }),
   { name: 'ByPOI' }
@@ -49,44 +66,107 @@ const useStyles = makeStyles(
 const ByPOI = props => {
   const classes = useStyles(props)
 
-  const [query, setQuery] = useState('')
+  const [loading, setLoading] = useState(true)
   const [searchCategory, setSearchCategory] = useState('')
+  const [address, setAddress] = useState('')
+  const [pois, setPois] = useState(null)
+  const [addressCoords, setAddressCoords] = useState(null)
 
-  //   useEffect(() => {
-  //     const getStuff = async () => {
-  //       const map = await getMapByAddress(query)
-  //       console.log(JSON.stringify(map, null, 2))
-  //     }
-  //     getStuff()
-  //   }, [])
+  const getPOIs = () => {
+    const getPoints = async () => {
+      setLoading(true)
+      const data = await getMapByPOI(searchCategory, address)
+      console.log('POIs: ', data.features)
+      setPois(data.features)
+      const coords = await getMapByAddress(address)
+      console.log('Address coords: ', coords.features)
+      setAddressCoords(coords.features)
+      setLoading(false)
+    }
 
-  //   const newMap = () => {
-  //     const getNewMap = async () => {
-  //       const map = await getMapByAddress(query)
-  //     }
-  //     getNewMap()
-  //   }
+    // const getMapCenter = async () => {
+    //   const coords = await getMapByAddress(address)
+    //   console.log('Address coords: ', coords.features)
+    //   setAddressCoords(coords.features)
+    //   setLoading(false)
+    // }
+
+    getPoints()
+    // getMapCenter()
+  }
+
   const handleChange = event => {
-    setSearchCategory(event.target.value)
+    switch (event.target.name) {
+      case 'Category':
+        setSearchCategory(event.target.value)
+        break
+      case 'Address':
+        setAddress(event.target.value)
+        break
+      default:
+    }
   }
 
   return (
     <div className={classes.root}>
-      {searchCategory}
-      <NativeSelect
-        value={searchCategory}
-        className={classes.categorySelect}
-        onChange={handleChange}
-        inputProps={{ name: 'Category' }}
-      >
-        {CATEGORIES.map((category, index) => {
-          return (
-            <option key={index} value={category}>
-              {category.charAt(0).toUpperCase() + category.slice(1)}
-            </option>
-          )
-        })}
-      </NativeSelect>
+      <Paper className={classes.searchContainer}>
+        {searchCategory}
+        <NativeSelect
+          name="Category"
+          value={searchCategory}
+          className={classes.categorySelect}
+          onChange={handleChange}
+          inputProps={{ name: 'Category' }}
+        >
+          {CATEGORIES.map((category, index) => {
+            return (
+              <option key={index} value={category}>
+                {category.charAt(0).toUpperCase() + category.slice(1)}
+              </option>
+            )
+          })}
+        </NativeSelect>
+
+        <TextField
+          fullWidth
+          name="Address"
+          value={address}
+          variant="standard"
+          className={classes.addressInput}
+          onChange={handleChange}
+        />
+
+        <Button variant="contained" color="primary" onClick={() => getPOIs()}>
+          Search
+        </Button>
+
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <Map
+            initialViewState={{
+              latitude: addressCoords[0].center[1],
+              longitude: addressCoords[0].center[0],
+              zoom: 12,
+            }}
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              flex: 1,
+              minHeight: '50vh',
+              width: '60vw',
+              marginTop: '1vh',
+              marginBottom: '2vh',
+            }}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+            mapboxAccessToken={ACCESS_TOKEN}
+          >
+            {pois.map((point, index) => {
+              return <Marker key={index} latitude={point.center[1]} longitude={point.center[0]} />
+            })}
+          </Map>
+        )}
+      </Paper>
     </div>
   )
 }
